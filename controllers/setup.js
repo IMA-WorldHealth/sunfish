fded1e57377a70957700229224704ec594d275d1
Jonathan Niles <jonathanwniles@gmail.com>
2019-10-26 09:24:49 +0100

    feat: move server config into SQLite3
    We've moved the configuration of the application from the .env file into
    the SQLite3 file.  The application will automatically detect if it is
    installed or not and provide a "setup" screen to allow users to set the
    application up. (Reapply)
    
    _NOTE: if the application is already set up, the setup variables can no
    longer be altered._
    
    Closes #18.

---
+++ controllers/setup.js
@@ -0,0 +1,59 @@
+const router = require('express').Router();
+const api = require('@ima-worldhealth/dhis2-api');
+const debug = require('debug')('sunfish:setup');
+const db = require('../lib/db');
+
+/**
+ * @function setup
+ * @method GET
+ *
+ * @description
+ * Renders the setup/configuration page for the application.  This will
+ * only be run on the first installation to set up the DHIS2 username,
+ * password and web API target.
+ */
+router.get('/', (req, res) => {
+  res.render('setup.pug');
+});
+
+/**
+ * @function setup
+ * @method POST
+ *
+ * @description
+ * Receives the setup from the client, tests it, and writes it the
+ * database if DHIS2 accepts the credentials.
+ */
+router.post('/', async (req, res) => {
+  const { server, username, password } = req.body;
+  api.configure({ url: server });
+
+  debug('Setting credentials for API');
+  try {
+    await api.auth.isValidCredentials(username, password);
+
+    debug('Credentials accepted.');
+
+    // login successful, register the credentials with the API
+    api.configure({ url: server, auth: { username, password } });
+
+    db.prepare('DELETE FROM credentials;').run();
+    db.prepare('INSERT INTO credentials (username, password, server) VALUES (?, ?, ?);')
+      .run(username, password, server);
+
+    debug('Credentials saved.');
+
+    req.flash('info', req.t('INSTALL.SUCCESS', { server, username }));
+    res.redirect('/');
+  } catch (error) {
+    // if an error is thrown, it means that the API does not have
+    // valid credentials.  We must inform the user to try again.
+
+    debug('An error occurred:', error);
+
+    req.flash('error', req.t('INSTALL.ERROR', error));
+    res.redirect('/setup');
+  }
+});
+
+module.exports = router;
